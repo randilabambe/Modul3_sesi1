605fef08ea1c9c80ec9ab43ac01f68cd6ba40ae0
randilabambe <randilabambe@gmail.com>
2022-03-25 21:53:25 +0800

    homework2_modul2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './berkas/berkas.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img src={data.album.images[0].url} width="300" height="300" className="rotate" />
+      </div>
+      <div>
+        <h2>{data.name}</h2>
+        <h3>{data.album.name}</h3>
+        <p>{data.artists[0].name}</p>
+	<button><a href={data.album.artists[0].external_urls.spotify}>select</a></button>
+      </div>
     </div>
   );
 }
