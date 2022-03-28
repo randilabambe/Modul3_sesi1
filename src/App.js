d0b133359808329ee8c764f9120443586e55da7d
randilabambe <randilabambe@gmail.com>
2022-03-28 19:53:14 +0800

    modul2_sesi3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import data from './berkas/berkas.js'
-import { AlbumImage, AlbumDesc, AlbumName, ButtonUrl } from './component/index.js';
+import ObjectLoop from './component/track.js'
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <img src={data.album.images[0].url} width="300" height="300" className="rotate" />
-      </div>
-      <div>
-        <h2>{data.name}</h2>
-        <h3>{data.album.name}</h3>
-        <p>{data.artists[0].name}</p>
-	<button><a href={data.album.artists[0].external_urls.spotify}>select</a></button>
-      </div>
+     <ObjectLoop/>
     </div>
   );
 }
